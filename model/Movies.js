b2754282ada6dfa20d0be853e380c764df049cfa
Giulia Melao <giulia.melao4@gmail.com>
2023-12-01 16:42:46 -0300

    Changing the structure to adapt Sequelize

--- model/Movies.js
+++ model/Movies.js
@@ -1,64 +1,117 @@
-let ids = 0;
-let movies = [];
+const { MoviesModel } = require('./bd')
+
 
 module.exports = {
-        save(title, year, director, genre) {
-                let obj = {id: ++ids, title: title, year: year, director: director, genre: genre}
-                movies.push(obj)
-                return obj
-        },
-        update(id, title, year, director, genre) {
-                let pos = this.getPositionById(id)
-                if (pos>= 0) {
-                        let obj = {id: id, title: title, year: year, director: director, genre: genre}
-                        movies[pos] = obj
-                        return movies[pos]
+        save: async (title, year, director, genre) => {
+                try {
+                    const movie = await MovieModel.create({
+                        title: title,
+                        year: year,
+                        director: director,
+                        genre: genre
+                    });
+                    return movie;
+                } catch (error) {
+                    console.error('Error while saving movie:', error);
+                    throw error;
                 }
-                return null
-        },
-        list(){
-                return movies
-        },
-        listByTitle(title) {
-                let lista = []
-                for (let i=0 ; i<movies.length ; i++) {
-                        if (movies[i].title.toUpperCase().startsWith(title.toUpperCase())) {
-                                lista.push(movies[i])
-                        }
-                }
-                return lista;
-        },
-        listByDirector(director) {
-                let lista = []
-                for (let i=0 ; i<movies.length ; i++) {
-                        if (movies[i].director.toUpperCase().startsWith(director.toUpperCase())) {
-                                lista.push(movies[i])
-                        }
-                }
-                return lista;
-        },
-        getPositionById(id) {
-                for (let i=0 ; i<movies.length ; i++) {
-                        if (movies[i].id == id) {
-                                return i;
-                        }
-                }
-                return -1;
-        },
-        getElementById(id) {
-                let pos = this.getPositionById(id)
-                if (pos >= 0) {
-                        return movies[pos];
-                }
-                return null;
-        },
-        delete(id) {
-                let i = this.getPositionById(id);
-                if (i >= 0) {
-                        movies.splice(i, 1);
-                        return true;
-                }
-                return false;
-        }
+            },
 
+        update: async (id, title, year, director, genre) => {
+                try {
+                        const movie = await MovieModel.findByPk(id);
+
+                        if (movie) {
+                        movie.title = title;
+                        movie.year = year;
+                        movie.director = director;
+                        movie.genre = genre;
+
+                        await movie.save();
+                        return movie;
+                        }
+                        return null;
+                } catch (error) {
+                        console.error('Error while updating movie:', error);
+                        throw error;
+                }
+        },
+
+        list: async () => {
+                try {
+                        return await MovieModel.findAll();
+                } catch (error) {
+                        console.error('Error fetching movies:', error);
+                        throw error;
+                }
+        },
+
+        listByTitle: async (title) => {
+                try {
+                        const movies = await MovieModel.findAll({
+                        where: {
+                                title: {
+                                [sequelize.Op.iLike]: `${title}%`
+                                }
+                        }
+                        });
+                        return movies;
+                } catch (error) {
+                        console.error('Error listing movies by title:', error);
+                        throw error;
+                }
+        },
+
+        listByDirector: async (director) => {
+                try {
+                        const movies = await MovieModel.findAll({
+                        where: {
+                                director: {
+                                [sequelize.Op.iLike]: `${director}%`
+                                }
+                        }
+                        });
+                        return movies;
+                } catch (error) {
+                        console.error('Error listing movies by director:', error);
+                        throw error;
+                }
+        },
+
+        getPositionById: async (id) => {
+                try {
+                        const movie = await MovieModel.findByPk(id);
+                        if (movie) {
+                        return movie.id;
+                        }
+                        return -1;
+                } catch (error) {
+                        console.error('Error:', error);
+                        throw error;
+                }
+        },
+
+        getElementById: async (id) => {
+                try {
+                        const movie = await MovieModel.findByPk(id);
+                        return movie || null;
+                } catch (error) {
+                        console.error('Error getting movie by id:', error);
+                        throw error;
+                }
+        },
+
+        delete: async (id) => {
+                try {
+                        const result = await MovieModel.destroy({
+                                where: {
+                                        movie_id: id
+                                }
+                        });
+                        return result > 0; // retorna true se deletar com sucesso
+                } catch (error) {
+                        console.error('Error while deleting movie:', error);
+                        throw error;
+                }
+        }
 }
