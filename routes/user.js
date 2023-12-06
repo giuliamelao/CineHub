e5413e171fcaa2f43fb216e1e2bd457b56c37fdc
Giulia Melao <giulia.melao4@gmail.com>
2023-12-06 18:16:48 -0300

    user route added

---
+++ routes/user.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+const Auth = require('../helpers/Auth');
+const { MovieModel, UserModel } = require('../model/bd');
+
+const Auth = require('../helpers/Auth');
+
+
+router.get('/user-page', Auth.validator, (req, res) => {
+    res.json({ message: 'Welcome to the User Page! 🎬 Here you can view the movie list and leave a note or review!' });
+});
+
+router.put('/user-page/edit-info/:id', Auth.validator, async (req, res) => {
+        try {
+                const userId = req.params.id;
+                const { password, nome } = req.body;
+
+                const updateUser = await UserModel.update(
+                        { password, nome },
+                        { where: { id: userId } }
+                );
+
+                if (updateUser[0]) {
+                        res.json({ mensagem: 'User updated successfully!' });
+                } else {
+                        res.status(404).json({ mensagem: 'User not found' });
+                }
+        } catch (error) {
+                console.error('Error updating user:', error);
+                res.status(500).json({ mensagem: 'Error updating user' });
+        }
+});
+
+
+module.exports = router;
