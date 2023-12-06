0ca90254058a6c360c5b407d76e10b4a160c0b17
Giulia Melao <giulia.melao4@gmail.com>
2023-12-06 18:17:42 -0300

    fixes and modifications

--- routes/admin.js
+++ routes/admin.js
@@ -8,24 +8,51 @@ router.get('/admin-page', Auth.validator, (req, res) => {
     res.json({ message: 'Welcome to the Admin Page! 👑 Here you can add and delete Moderators' });
 });
 
-router.post('/admin-page/add-moderator', Auth.validator, async (req, res) => {
+
+router.post('/admin-page/add-admin', Auth.isAdmin, async (req, res) => {
         try {
-            const { nome, username, password } = req.body;
-            const newModerator = await UserModel.create({
-                nome,
-                username,
-                password,
-                role: 'moderator',
-            });
-    
-            res.json({ message: 'Moderator added successfully!', moderator: newModerator });
+                const { nome, username, password } = req.body;
+                const newAdmin = await UserModel.create({
+                        nome,
+                        username,
+                        password,
+                        role: 'admin',
+                });
+
+                res.json({ message: 'Admin added successfully!', admin: newAdmin });
         } catch (error) {
-            console.error('Error adding moderator:', error);
-            res.status(500).json({ mensagem: 'Error adding moderator' });
+                console.error('Error adding admin:', error);
+                res.status(500).json({ mensagem: 'Error adding admin' });
         }
-});
+
+    });
     
-router.delete('/admin-page/delete-moderator/:id', Auth.validator, async (req, res) => {
+
+
+
+router.post('/admin-page/add-moderator', Auth.isAdmin, async (req, res) => {
+        try {
+                const { nome, username, password } = req.body;
+                const newModerator = await UserModel.create({
+                        nome,
+                        username,
+                        password,
+                        role: 'moderator',
+                });
+
+                res.json({ message: 'Moderator added successfully!', moderator: newModerator });
+        } catch (error) {
+                console.error('Error adding moderator:', error);
+                res.status(500).json({ mensagem: 'Error adding moderator' });
+        }
+
+    });
+    
+
+
+
+    
+router.delete('/admin-page/delete-moderator/:id', Auth.isAdmin, async (req, res) => {
         try {
                 const moderatorId = req.params.id;
                 const deletedModerator = await UserModel.destroy({
@@ -48,7 +75,7 @@ router.delete('/admin-page/delete-moderator/:id', Auth.validator, async (req, re
 
 
 
-router.delete('/admin-page/delete-user/:id', Auth.validator, async (req, res) => {
+router.delete('/admin-page/delete-user/:id', Auth.isAdmin, async (req, res) => {
         try {
                 const userId = req.params.id;
                 const deletedUser = await UserModel.destroy({
@@ -66,6 +93,27 @@ router.delete('/admin-page/delete-user/:id', Auth.validator, async (req, res) =>
         } catch (error) {
                 console.error('Error deleting moderator:', error);
                 res.status(500).json({ mensagem: 'Error deleting moderator' });
+        }
+});
+
+router.put('/admin-page/edit-user-info/:id', Auth.isAdmin, async (req, res) => {
+        try {
+                const userId = req.params.id;
+                const { password, nome } = req.body;
+
+                const updateUser = await UserModel.update(
+                        { password, nome },
+                        { where: { id: userId } }
+                );
+
+                if (updateUser[0]) {
+                        res.json({ mensagem: 'User updated successfully!' });
+                } else {
+                        res.status(404).json({ mensagem: 'User not found' });
+                }
+        } catch (error) {
+                console.error('Error updating user:', error);
+                res.status(500).json({ mensagem: 'Error updating user' });
         }
 });
 
