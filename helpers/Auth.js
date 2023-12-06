ade08c7e797346aac985917844d04a18d4652099
Giulia Melao <giulia.melao4@gmail.com>
2023-12-06 18:14:33 -0300

    middleware func isAdmin added

--- helpers/Auth.js
+++ helpers/Auth.js
@@ -14,5 +14,20 @@ module.exports = {
                                 next()
                         }
                 })
+        },
+
+        isAdmin: (req, res, next) => {
+                let beartoken = req.headers.authorization
+                let token = beartoken.split(" ")
+                if (token[0] == 'Bearer') {
+                        token = token[1]
+                }
+                jwt.verify(token, '123456', (err, obj) => {
+                        if (err || obj.role !== 'admin') res.status(403).json({mensagem: "Invalid Token or unauthorized"})
+                        else {
+                                req.user = obj.user
+                                next()
+                        }
+                })
         }
 }
